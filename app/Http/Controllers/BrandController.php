f2378b576ce145ca6eeb8be6333a42294ee569bb
Skye Jacobs <skyejacobs@casema.nl>
2025-10-16 14:43:31 +0200

    Added controller & model

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function index()
+    {
+        return view('brands.index');
+    }
+
+    public function show(Brand $brand)
+    {
+        return view('brands.show', compact('brand'));
+    }
+
+    public function create()
+    {
+        return view('brands.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'year_founded' => 'required|digits:4|integer|min:1800|max:' . (date('Y')),
+            'headquarters' => 'required|string|max:255',
+            'founder' => 'required|string|max:255',
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->input('name');
+        $brand->description = $request->input('description');
+        $brand->year_founded = $request->input('year_founded');
+        $brand->headquarters = $request->input('headquarters');
+        $brand->founder = $request->input('founder');
+
+        $brand->save();
+
+        return redirect()->route('brands.show', $brand)->with('success', 'Brand created successfully.');
+    }
+}
